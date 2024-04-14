e4f0e6fb60a4b2c91bab7aa8a5fb91a3704a51bb
batm192 <155922446+batm192@users.noreply.github.com>
2024-04-14 20:00:55 +0900

    月の表示を修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
-const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
-console.log(year + '年' + month + '月' + day + '日');
+const toDay = new Date();
+
+let month = toDay.getMonth() + 1;
+let date = toDay.getDate();
+let year = toDay.getFullYear();
+
+console.log(year + '年' + month + '月' + date + '日');
